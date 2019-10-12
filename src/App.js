a63b250026172ffdcb80ffa25fdae15e384d62e3
Oventura19 <47031203+Oventura19@users.noreply.github.com>
2019-10-11 19:28:18 -0500

    filtrando-posts
    Filtrando Posts

--- src/App.js
+++ src/App.js
@@ -2,20 +2,42 @@ import React, { Component } from 'react';
 import posts from './posts'
 
 
-// Modifica el componente App para implmentar la funcionalidad requerida
+// Modifica el componente App para implementar la funcionalidad requerida
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      everyPosts: posts,
+      posts: posts
+    }
+  }
+
+  handleChange(e) {
+    let actualList = [];
+    let newList =[];
+
+    actualList = this.state.everyPosts;
+    newList = actualList.filter((item, index) => item.title.toLowerCase().includes(e.target.value.toLowerCase() ));
+
+    this.setState({
+      posts: newList
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" onChange={this.handleChange.bind(this)} placeholder="Ingresa el término de búsqueda"/>
         </div>
         <ul>
+          {this.state.posts.map((post, index) => (
           <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
+            <a href={posts[0].url}><img src={posts[0].image}/></a>
+            <p>{posts[0].title}</p>
           </li>
+          ))}
         </ul>
       </div>
     )
